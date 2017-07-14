fbf5c6fc7843be22fd4dd5885152c863346d2891
Thomas Andreo <thomas.andreo@fansentertainment.com>
2017-07-14 15:59:45 -0400

    [0.7.x] Fixed Link component

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 import RouteFactory from '../routeFactory';
 
 
-const Link = ({ label, to, navigate, routes }) => {
-  const destination = RouteFactory.parse(to);
+const Link = ({ label, to, navigate }) => {
+  const { path } = RouteFactory.parse(to);
   const handleClick = (e) => {
     e.preventDefault();
-    navigate(destination);
+    navigate(path);
   };
 
   return <a href={to} onClick={handleClick}>{label}</a>;
@@ -17,7 +17,6 @@ Link.propTypes = {
   label: PropTypes.string.isRequired,
   to: PropTypes.string.isRequired,
   navigate: PropTypes.func.isRequired,
-  routes: PropTypes.arrayOf(PropTypes.shape()).isRequired,
 };
 
 export default Link;
