eea20e084766041e52c73c8ef5f8cb4f594b0820
Justin D'Errico <justin.derrico1991@gmail.com>
2017-07-21 09:25:47 -0400

    added example

--- src/components/Provider.jsx
+++ src/components/Provider.jsx
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { /*navComplete,*/ init } from '../actions';
 import RouteFactory from '../routeFactory';
+import MiddlewareController from '../middlewareController';
 
 
 class FansRouterProvider extends Component {
   constructor(props, context) {
     super(props, context);
-    const { router } = props;
+    const { router, middlewares } = props;
     this.history = router.history;
     this.routes = router.routes;
     /* this.history.listen((location, action) => {
@@ -15,8 +16,10 @@ class FansRouterProvider extends Component {
      *   context.store.dispatch(navComplete(route, action));
      * });
      */
+    MiddlewareController.init(middlewares, this.routes, this.history);
     const { location } = this.history;
     const route = RouteFactory.parse(location.pathname.concat(location.search));
+
     context.store.dispatch(init(route, this.routes));
   }
 
@@ -35,6 +38,7 @@ class FansRouterProvider extends Component {
 FansRouterProvider.propTypes = {
   children: PropTypes.element.isRequired,
   router: PropTypes.shape().isRequired,
+  middlewares: PropTypes.arrayOf(PropTypes.shape()).isRequired,
 };
 
 FansRouterProvider.contextTypes = {
