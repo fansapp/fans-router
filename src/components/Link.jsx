b0c511566da63152996c9f383d7b18ba336f90e6
Thomas Andreo <thomas.andreo@gmail.com>
2017-07-24 15:58:15 -0400

    Implemented base class for Link (#41)

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,25 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import RouteFactory from '../routeFactory';
 
 
-const Link = ({ children, className, to, navigate }) => {
+const Link = ({ baseClass, children, className, to, navigate, routeName }) => {
   const handleClick = (e) => {
     e.preventDefault();
     navigate(to);
   };
 
-  return <a className={className} href={to} onClick={handleClick}>{children}</a>;
+  const { name } = RouteFactory.parse(to);
+  let classes = baseClass;
+  if (name === routeName) {
+    classes = classes.concat(` ${baseClass}--active`);
+  }
+
+  return (
+    <a className={classes.concat(` ${className}`)} href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 };
 
 Link.defaultProps = {
-  className: null,
+  baseClass: 'Link',
+  className: '',
 };
 
 Link.propTypes = {
+  baseClass: PropTypes.string,
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
   to: PropTypes.string.isRequired,
   navigate: PropTypes.func.isRequired,
+  routeName: PropTypes.string.isRequired,
 };
 
 export default Link;
