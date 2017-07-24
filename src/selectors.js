080724865dd393e9a4e4b45b5a83977b5c4444b1
Justin D'Errico <justin.derrico1991@gmail.com>
2017-07-24 13:34:54 -0400

    1.1.0 documentation

--- src/selectors.js
+++ src/selectors.js
@@ -4,9 +4,9 @@ export default {
   getRouteName: (state) => state.router.route.name,
   getRoutePath: (state) => state.router.route.path,
   getQuery: (state) => state.router.route.query,
-  getQueryParam: (state, name) => name in state.router.route.query ?
-    state.router.route.query[name] : null,
-  getParam: (state, name) => name in state.router.route.params ?
-    state.router.route.params[name] : null,
+  getQueryParam: (state, key) => key in state.router.route.query ?
+    state.router.route.query[key] : null,
+  getParam: (state, key) => key in state.router.route.params ?
+    state.router.route.params[key] : null,
   getParams: (state) => state.router.route.params,
 };
