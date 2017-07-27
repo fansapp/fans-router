9ac2d1f39e5691e669fc4f367025479e6b68ce47
Thomas Andreo <thomas.andreo@fansentertainment.com>
2017-07-27 13:54:52 -0400

    Fixed active subroute state in Link

--- src/tests/Link.spec.js
+++ src/tests/Link.spec.js
@@ -1,64 +1,60 @@
 import React from 'react';
-import { expect, assert } from 'chai';
+import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import sinon from 'sinon';
 
-import RouteFactory from '../routeFactory';
+// import RouteFactory from '../routeFactory';
 import Link from '../components/Link';
 
 describe('<Link />', () => {
-  let RouteFactoryStub;
-  beforeEach(() => {
-    RouteFactoryStub = sinon.stub(RouteFactory, 'parse').callsFake((to) => {
-      switch (to) {
-        case '/about':
-          return { path: '/about', name: 'root.about' };
-        default:
-          return null;
-      }
-    });
-  });
-
-  afterEach(() => {
-    RouteFactoryStub.restore();
-  });
+  const baseRoute = { path: '', name: '', params: {}, query: {} };
+  const aboutRoute = { ...baseRoute, path: '/about', name: 'root.about' };
 
   it('renders successfully', () => {
-    const wrapper = shallow(<div><Link navigate={() => true} routeName={'myRoute'} to={'/about'}>link</Link></div>);
+    const wrapper = shallow(<div><Link navigate={() => true} route={aboutRoute} to="/about">link</Link></div>);
     expect(wrapper.find(Link)).to.have.length(1);
   });
 
   it('renders its children', () => {
-    const wrapper = shallow(<Link navigate={() => true} routeName={'myRoute'} to={'/about'}><p id="child">child</p></Link>);
+    const wrapper = shallow(<Link navigate={() => true} route={aboutRoute} to="/about"><p id="child">child</p></Link>);
     expect(wrapper.find('#child')).to.have.length(1);
   });
 
   it('implements default base class', () => {
-    const wrapper = shallow(<Link navigate={() => true} routeName={'myRoute'} to={'/about'}>link</Link>);
-    assert(wrapper.node.props.className.includes('Link'));
+    const wrapper = shallow(<Link navigate={() => true} route={aboutRoute} to="/about">link</Link>);
+    expect(wrapper.node.props.className).to.include('Link');
   });
 
   it('implements className', () => {
     const myClass = 'myClass';
-    const wrapper = shallow(<Link navigate={() => true} className={myClass} routeName={'myRoute'} to={'/about'}>link</Link>);
-    assert(wrapper.node.props.className.includes(myClass));
-  });
-
-  it('implements default active class', () => {
-    const wrapper = shallow(<Link navigate={() => true} routeName={'root.about'} to={'/about'}>link</Link>);
-    assert(wrapper.node.props.className.includes('Link--active'));
-  });
-
-  it('implements custom base class active class', () => {
-    const baseClass = 'custom';
-    const wrapper = shallow(<Link navigate={() => true} baseClass={baseClass} routeName={'root.about'} to={'/about'}>link</Link>);
-    assert(wrapper.node.props.className.includes(`${baseClass}--active`));
+    const wrapper = shallow(<Link navigate={() => true} className={myClass} route={aboutRoute} to="/about">link</Link>);
+    expect(wrapper.node.props.className).to.include(myClass);
   });
 
   it('calls navigate when clicked', () => {
     const onNavigate = sinon.spy();
-    const wrapper = shallow(<Link navigate={onNavigate} routeName={'root.about'} to={'/about'}>link</Link>);
+    const wrapper = shallow(<Link navigate={onNavigate} route={aboutRoute} to="/about">link</Link>);
     wrapper.simulate('click', { preventDefault() {} });
-    assert(onNavigate.called);
+    expect(onNavigate.called).to.be.true;
+  });
+
+  it('implements default active class', () => {
+    const wrapper = shallow(<Link navigate={() => true} route={aboutRoute} to="/about">link</Link>);
+    expect(wrapper.node.props.className).to.include('Link--active');
+  });
+
+  it('implements custom base class active class', () => {
+    const baseClass = 'custom';
+    const wrapper = shallow(<Link navigate={() => true} baseClass={baseClass} route={aboutRoute} to="/about">link</Link>);
+    expect(wrapper.node.props.className).to.include(`${baseClass}--active`);
+  });
+
+  it('implements active class on subroutes', () => {
+    const route = { ...baseRoute, path: '/posts/1', name: 'root.post' };
+    const wrapper1 = shallow(<Link navigate={() => true} route={route} to="/posts/1">link</Link>);
+    expect(wrapper1.node.props.className).to.include('Link--active');
+
+    const wrapper2 = shallow(<Link navigate={() => true} route={route} to="/posts/2">link</Link>);
+    expect(wrapper2.node.props.className).to.not.include('Link--active');
   });
 });
