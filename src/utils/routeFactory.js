2c943951122296b141f2121f6a01e211df95ebc5
Thomas Andreo <thomas.andreo@gmail.com>
2017-08-03 15:16:53 -0400

    Revert "Root query hotfix (#50)"
    This reverts commit 65e1b7eb4305eaa5c8b480796e51398f9af97a85.

--- src/utils/routeFactory.js
+++ src/utils/routeFactory.js
@@ -8,11 +8,11 @@ import memoize from 'lru-memoize';
  */
 export const makeRouteObject = memoize(100, null, true)(
   (name, path, query = {}, params = {}) => ({
-    name,
-    path,
-    params,
-    query,
-  }));
+  name,
+  path,
+  params,
+  query,
+}));
 
 
 /**
@@ -22,9 +22,9 @@ export const makeRouteObject = memoize(100, null, true)(
  */
 export const hasUnexpectedQueryType = memoize(100, null, true)(
   (query) => (
-    Object.keys(query)
-      .some(r => query[r] && ['object', 'function'].includes(typeof query[r]))
-  ));
+  Object.keys(query)
+    .some(r => query[r] && ['object', 'function'].includes(typeof query[r]))
+));
 
 
 /**
@@ -37,10 +37,10 @@ export const hasUnexpectedQueryType = memoize(100, null, true)(
  */
 export const replacePathParamsByValues = memoize(100, null, true)(
   (path, params, requiredParams) => (
-    requiredParams.reduce((builtPath, curReqParam) => (
-      builtPath.replace(curReqParam, params[curReqParam.slice(1, curReqParam.length)])
-    ), path)
-  ));
+  requiredParams.reduce((builtPath, curReqParam) => (
+    builtPath.replace(curReqParam, params[curReqParam.slice(1, curReqParam.length)])
+  ), path)
+));
 
 
 /**
@@ -51,15 +51,15 @@ export const replacePathParamsByValues = memoize(100, null, true)(
  */
 export const validateObjectPathParams = memoize(100, null, true)(
   (params, requiredParams) => (
-    requiredParams.map(p => p.slice(1, p.length))
-      .reduce((parameters, currentParam) => {
-        if (!params.hasOwnProperty(currentParam)) {
-          throw new Error(currentParam);
-        }
+  requiredParams.map(p => p.slice(1, p.length))
+    .reduce((parameters, currentParam) => {
+      if (!params.hasOwnProperty(currentParam)) {
+        throw new Error(currentParam);
+      }
 
-        return { ...parameters, [currentParam]: String(params[currentParam]) };
-      }, {})
-  ));
+      return { ...parameters, [currentParam]: String(params[currentParam]) };
+    }, {})
+));
 
 
 /**
@@ -72,41 +72,41 @@ export const validateObjectPathParams = memoize(100, null, true)(
  */
 export const validateStringPath = memoize(100, null, true)(
   (nodes = [], currentNode, params = {}, name = 'root') => {
-    const newParams = params;
+  const newParams = params;
 
-    if (nodes.length === 0) {
-      return {
-        params,
-        name,
-      };
+  if (nodes.length === 0) {
+    return {
+      params,
+      name,
+    };
+  }
+
+  const nextNode = currentNode.routes.find((r) => {
+    const path = r.path.replace(/^\/|\/$/g, '');
+
+    if (path.startsWith(':')) {
+      if (!nodes[0]) {
+        throw new Error(nodes[0]);
+      }
+      newParams[path.slice(1, path.length)] = nodes[0];
+      return r;
     }
 
-    const nextNode = currentNode.routes.find((r) => {
-      const path = r.path.replace(/^\/|\/$/g, '');
-
-      if (path.startsWith(':')) {
-        if (!nodes[0]) {
-          throw new Error(nodes[0]);
-        }
-        newParams[path.slice(1, path.length)] = nodes[0];
-        return r;
-      }
-
-      if (path === nodes[0]) {
-        return r;
-      }
-
-      return false;
-    });
-
-    if (!nextNode) {
-      throw new Error(nodes[0]);
+    if (path === nodes[0]) {
+      return r;
     }
 
-    return validateStringPath(
-      nodes.slice(1, nodes.length),
-      nextNode,
-      newParams,
-      name.concat(`.${nextNode.name}`)
-    );
+    return false;
   });
+
+  if (!nextNode) {
+    throw new Error(nodes[0]);
+  }
+
+  return validateStringPath(
+    nodes.slice(1, nodes.length),
+    nextNode,
+    newParams,
+    name.concat(`.${nextNode.name}`)
+  );
+});
