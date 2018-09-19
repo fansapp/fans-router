e6a6c494c6448874acf49c91c02da0f5c822d983
Miles Rostami <miles.rostami@gmail.com>
2018-09-19 11:36:52 -0400

    removed force option from demo sidebar link

--- demo/src/app/common/components/SideBar/index.jsx
+++ demo/src/app/common/components/SideBar/index.jsx
@@ -10,7 +10,7 @@ const SideBar = () => (
     <div className="SideBar__navigation">
       <ul>
         <li className="SideBar__link"><Link to="/">Home</Link></li>
-        <li className="SideBar__link"><Link force to="/posts">Posts</Link></li>
+        <li className="SideBar__link"><Link to="/posts">Posts</Link></li>
         <li className="SideBar__link"><Link to="/login">Login</Link></li>
         <li className="SideBar__link"><Link to="/secret">Secret</Link></li>
       </ul>
