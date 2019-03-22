73c178ff4a1618a236ed33dd4298b10098c340f2
Thomas Andreo <thomas.andreo@fansentertainment.com>
2019-03-22 11:26:02 -0400

    Removed HMR integration

--- demo/src/app/core/components/AppContainer.jsx
+++ demo/src/app/core/components/AppContainer.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { AppContainer } from 'react-hot-loader';
 
 import store from '../redux/store';
 
 
 export default (Component) => {
   ReactDOM.render(
-    <AppContainer>
       <Provider store={store()}>
         {Component}
       </Provider>
-    </AppContainer>,
     document.getElementById('app')
   );
 };
--- demo/src/server/routes.js
+++ demo/src/server/routes.js
@@ -1,17 +1,11 @@
 import express from 'express';
 import fs from 'fs';
 import path from 'path';
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-
 import buildConfig from '../dev/build/config';
 import mw from './middlewares';
 
-import webpackConfig from '../dev/webpack/config.dev';
 
 const { SERVER_PORT } = process.env;
-
 
 // Get hash from this build
 let hash = '';
@@ -23,23 +17,7 @@ if (buildConfig.MINIMIZE) {
   hash = `?${hashKey}` || '';
 }
 
-
-const compiler = webpack(webpackConfig);
-
 export default (app) => {
-  if (process.env.HOT === 'true') {
-    // Start a webpack-dev-server
-    app.use(webpackDevMiddleware(compiler, {
-      // server and middleware options
-      publicPath: webpackConfig.output.publicPath,
-      noInfo: true,
-      headers: { 'Access-Control-Allow-Origin': '*' },
-    }));
-
-    // Enables HMR
-    app.use(webpackHotMiddleware(compiler));
-  }
-
   app.use('/public', express.static('dist'));
 
   app.listen(SERVER_PORT || 9000);
