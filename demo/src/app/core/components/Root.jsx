1cd57bc5434078722137c2e75f89d57eca18ed2e
Thomas Andreo <thomas.andreo@fansentertainment.com>
2019-03-22 14:13:51 -0400

    Updated demo for 4.0.0

--- demo/src/app/core/components/Root.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import App from '../../common/containers/App';
-import '../../../scss/app.scss';
-
-
-export default <App />;
--- demo/src/app/index.js
+++ demo/src/app/index.js
@@ -1,7 +1,22 @@
-import Root from './core/components/Root';
-import render from './core/components/AppContainer';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createRouter, Provider as RouterProvider } from '../../../dist';
+import routes from './core/router/routes';
+import middlewares from './core/router/middlewares';
+import initStore from './core/redux/store';
+import App from './common/components/App';
 
 
+/* eslint-disable react/jsx-filename-extension */
 document.addEventListener('DOMContentLoaded', () => {
-  render(Root);
+  const store = initStore();
+  ReactDOM.render(
+    <Provider store={store}>
+      <RouterProvider router={createRouter(routes)} middlewares={middlewares} store={store}>
+        <App />
+      </RouterProvider>
+    </Provider>,
+    document.getElementById('app')
+  );
 });
